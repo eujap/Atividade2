7547351be55907e2ce5d905ad8f69971c027c67b
Jose Augusto Pereira <116526589+eujap@users.noreply.github.com>
2024-03-16 21:47:50 -0400

    implementado a fuinção de sortear o numero

--- numerosrandom/componentes/Form/index.js
+++ numerosrandom/componentes/Form/index.js
@@ -1,25 +1,71 @@
-import React from "react";
+import React, {useState} from "react";
 import { View, Text, TextInput, Button } from "react-native";
-import numero
+
+
 
 export default function Form(){
+
+const [num1, setNum1]= useState(null)
+const [num2, setNum2]= useState(null)
+const [mensagemNum, setMensagemNum]= useState("Preencha primeiro e uiltimo numero!")
+const [numeroSorteado, setNumeroSorteado]= useState(null)
+const [textButton, setTextButton ]= useState("Sortear")
+const [erro, setErro] = useState("");
+
+function NumeroAleatorioIntervalo(num1, num2){
+    return Math.floor(Math.random() * (num2 - num1 +1)) + num1
+}
+
+function validacao(){
+    if(num1 != null && num2 != null && num1 != num2){
+        const novoNumeroSorteado = NumeroAleatorioIntervalo(parseInt(num1),parseInt(num2));  
+        setNumeroSorteado(novoNumeroSorteado)           
+        setMensagemNum("O Numero da Sorte é:")        
+        setTextButton("Sortear Novamente? ")
+        setNum1(null)
+        setNum2(null)
+        return
+    }
+    setNumeroSorteado(null)
+    setTextButton("Sortear")
+    setMensagemNum("Preencha primeiro e uiltimo numero!")
+}
+
+
     return(
         <View>
             <View>
                 <Text>Primeiro numero</Text>
                 <TextInput
+                    onChangeText={setNum1}
+                    value={num1}
                     placeholder="Digite um numero inteiro"
                     keyboardType="numeric"
                 />
                 <Text>Ultimo numero</Text>
                 <TextInput
+                    onChangeText={setNum2}
+                    value={num2}
                     placeholder="Digite um numero inteiro"
                     keyboardType="numeric"
                     />
-                    <Button title="Sortear"/>
+                    <Button onPress={() =>{
+                        if (num1 !== null && num2 !== null) {
+                            validacao();
+        } else {
+            setErro("Campos não preenchidos");
+        }
+                    } } 
+                    title={textButton} />
             </View>
-            <View><numero</View>
-            <View></View>
+            <View>{numeroSorteado !== null ? (
+          <Text>{mensagemNum} {numeroSorteado}</Text>
+        ) : (
+          <Text>{mensagemNum}</Text> )}</View>
+          <View>
+    <Text>{erro}</Text>
+</View>
+
         </View>
     )
 };
